4b12b78dee6992911211c6e5b379794846aff974
Burak Mete <burak.mete@tum.de>
2025-07-23 10:33:43 +0200

    :sparkles: Initial commit (#1)
    Includes an implementation of the SW and HW handlers

---
+++ src/adapters/mqss_adapter.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+
+class MQSSAdapter(ABC):
+    def __init__(self, config):
+        self.config = config
+
+    @abstractmethod
+    def run_circuit(self, circuit, *params):
+        # TODO: To be implemented by the child classes
+        pass
---
+++ src/api/interface.py
@@ -0,0 +1,6 @@
+{
+    "benchmark_type": "HW",
+    "benchmark_name": "randomized_benchmarking",
+    "backend": "pennylane",
+    "params": {"num_qubits": 2, "shots": 100, "token": "<MQSS-TOKEN>"},
+}

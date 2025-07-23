b62306f087e76ae8f163e566bcf35e5d219cff38
Bmete7 <burak.mete@tum.de>
2025-07-23 14:09:20 +0200

    :sparkles: Implement pennylane adapter

---
+++ src/adapters/pennylane_adapter.py
@@ -0,0 +1,32 @@
+from mqss.pennylane_adapter.device import MQSSPennylaneDevice
+from src.adapters.mqss_adapter import MQSSAdapter
+from src.adapters.config import MQSS_TOKEN
+
+
+class PennyLaneAdapter(MQSSAdapter):
+    def __init__(self, config):
+        super().__init__(config)
+        self.device = MQSSPennylaneDevice(
+            # wires=config["wires"],
+            # token=config["credentials"]["mqss_token"],
+            # backends=[config["backend"]],
+            wires=2,
+            token=MQSS_TOKEN,
+            backends="QExa20",
+        )
+
+    def run_circuit(self, circuit, params=None):
+        """Given a PennyLane circuit, run it using the PennylaneAdapter
+
+        Args:
+            circuit (qml.qnode): Pennylane Circuit
+            params (float, optional): Parameters to the circuit. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
+        qnode = circuit(self.device)
+        if params is not None:
+            return qnode(*params)
+        else:
+            return qnode()

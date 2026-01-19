2f7baa64395916cb15495b1dc91dd3a9ccdb762d
ctminh <minh.chung@lrz.de>
2026-01-19 16:51:47 +0100

    editted the time format in the job script

--- test/hpcqc_jobs/jobscript_ghz_example_timestamp.cmd
+++ test/hpcqc_jobs/jobscript_ghz_example_timestamp.cmd
@@ -12,20 +12,19 @@ t1=$(date +%s%3N)
 source ~/export_conda_env.sh
 t2_timestamp=$(date +"%a %d-%m-%Y %H:%M:%S.%3N")
 t2=$(date +%s%3N)
-echo "$t1_timestamp: Start loading conda env"
-echo "$t2_timestamp: Finished loading conda env"
-echo "-------------------------------------------------"
-echo "Elapsed time to load conda env (ms): $((t2 - t1))"
-echo "-------------------------------------------------"
+echo "[SBATCH] $t1_timestamp: Start loading conda env"
+echo "[SBATCH] $t2_timestamp: Finished loading conda env"
+echo "[SBATCH] -------------------------------------------------"
+echo "[SBATCH] Elapsed time to load conda env (ms): $((t2 - t1))"
+echo "[SBATCH] -------------------------------------------------"
 
 t_start_timestamp=$(date +"%a %d-%m-%Y %H:%M:%S.%3N")
 t_start=$(date +%s%3N)
 ~/conda/x86_64/bin/python ghz_example_timestamp.py --backend=QExa20
 t_end_timestamp=$(date +"%a %d-%m-%Y %H:%M:%S.%3N")
 t_end=$(date +%s%3N)
-echo "$t_start_timestamp: Start execution"
-echo "$t_end_timestamp: Finished execution"
-echo "-------------------------------------------------"
-echo "Elapsed time to execute (ms): $((t_end - t_start))"
-echo "-------------------------------------------------"
-
+echo "[SBATCH] $t_start_timestamp: Start execution"
+echo "[SBATCH] $t_end_timestamp: Finished execution"
+echo "[SBATCH] -------------------------------------------------"
+echo "[SBATCH] Elapsed time to execute (ms): $((t_end - t_start))"
+echo "[SBATCH] -------------------------------------------------"

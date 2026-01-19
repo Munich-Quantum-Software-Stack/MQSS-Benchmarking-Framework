12fa8a84806ec435010fecf69661abd23358b821
Bmete7 <burak.mete@tum.de>
2026-01-19 18:49:26 +0100

    :sparkles: add hybrid benchmark executor

--- src/mqssbench/framework/benchmark_executor.py
+++ src/mqssbench/framework/benchmark_executor.py
@@ -3,8 +3,11 @@
 from abc import ABC, abstractmethod
 from typing import List
 
+from mqssbench.framework.circuit_generator import CircuitGenerator
+
 from .types import CircuitSpec, RunContext, ExecutionResult
 from scipy.optimize import minimize
+import time
 
 
 class BenchmarkExecutor(ABC):
@@ -58,48 +61,86 @@ class DefaultBenchmarkExecutor(BenchmarkExecutor):
         return results
 
 
+def maxcut_expectation(counts: dict, edges: list[tuple[int, int]]) -> float:
+    """
+    Calculate the MaxCut expectation value for a given bitstring and edge list.
+    Each edge contributes +1 if the bits are different, 0 otherwise.
+    """
+    value = 0
+    for bitstring, count in counts.items():
+        for i, j in edges:
+            if bitstring[i] != bitstring[j]:
+                value += count
+            # else:
+            #     value -= count
+    value /= sum(counts.values())
+    return value
+
+
 class HybridBenchmarkExecutor(BenchmarkExecutor):
     """Hybrid benchmark executor for circuits that require both quantum and classical processing."""
 
     def run(
-        self, circuits: List[CircuitSpec], context: RunContext
+        self, generator: CircuitGenerator, context: RunContext
     ) -> List[ExecutionResult]:
         """Execute hybrid circuits using the adapter from context."""
         results: List[ExecutionResult] = []
-        for spec in circuits:
-            circuit_payload = spec.circuit
-            if "num_qubits" not in spec.metadata:
-                raise ValueError(
-                    "CircuitSpec.metadata must include a 'num_qubits' entry."
-                )
-            num_qubits = spec.metadata["num_qubits"]
+        context.params["expval"] = []
+        iteration = 0
+        start = time.time()
+        objective_values = []
 
-            run_result: ExecutionResult
+        def objective(x):
+            nonlocal iteration
 
-            if callable(circuit_payload):
-                minimize(
-                    context.adapter.execute_circuit,
-                    context.params["parameters"],
-                    args=(
-                        context,
-                        circuit_payload,
-                        num_qubits,
-                    ),
-                )
-                run_result = context.adapter.execute_circuit(
-                    context, circuit_payload, num_qubits=num_qubits
-                )
-            else:
-                run_result = context.adapter.execute_circuit(
-                    context, lambda: circuit_payload, num_qubits=num_qubits
+            params = context.params
+
+            params["parameters"] = x
+
+            circuits = generator.generate(params)
+
+            results = []
+            for spec in circuits:
+                result = context.adapter.execute_circuit(
+                    context,
+                    lambda: spec.circuit,
+                    num_qubits=spec.metadata["num_qubits"],
                 )
 
-            # set metadata in in new instance for immutability
-            run_result = ExecutionResult(
-                job_id=run_result.job_id,
-                counts=run_result.counts,
-                profiling_metrics=run_result.profiling_metrics,
-                metadata=dict(spec.metadata),
-            )
-            results.append(run_result)
+                objective_values.append(result)
+                result = maxcut_expectation(result.counts, spec.metadata["edges"]) * -1
+                context.params["expval"].append(result)
+                end = time.time()
+                print(f"Time elapsed: {end - start} seconds")
+                print(f"Iteration {iteration}: Exp. value: {result}")
+                iteration += 1
+                results.append(result)
+
+            return results[0]
+
+        res = minimize(
+            objective,
+            x0=context.params["parameters"],
+            method="COBYLA",
+            options={"maxiter": 250},
+        )
+
+        params = context.params
+        params["parameters"] = res.x.tolist()
+
+        circuits = generator.generate(params)
+
+        circuits[0].metadata["result"] = res.fun.tolist()
+        circuits[0].metadata["opt_params"] = res.x.tolist()
+
+        # set metadata in in new instance for immutability
+        run_result = ExecutionResult(
+            job_id=objective_values[-1].job_id,
+            counts=objective_values[-1].counts,
+            profiling_metrics=objective_values[-1].profiling_metrics,
+            metadata=dict(circuits[0].metadata),
+            exp_value=res.fun.tolist(),
+            optimal_params=res.x.tolist(),
+        )
+        results.append(run_result)
         return results

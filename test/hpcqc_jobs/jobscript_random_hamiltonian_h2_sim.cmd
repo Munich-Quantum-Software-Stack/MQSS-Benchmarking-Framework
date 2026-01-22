0d9cc18eb2688bdd2ef195c0bd658516af6b8ef2
ctminh <minh.chung@lrz.de>
2026-01-22 10:56:21 +0100

    updated h2 sim

--- test/hpcqc_jobs/jobscript_random_hamiltonian_h2_sim.cmd
+++ test/hpcqc_jobs/jobscript_random_hamiltonian_h2_sim.cmd
@@ -3,32 +3,53 @@
 #SBATCH -o ./%x_%j.out
 #SBATCH -e ./%x_%j.err
 #SBATCH --ntasks=1
-#SBATCH --gres=qpu:1
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=wolpy
 #SBATCH --time=00:30:00
 
-echo '---------------------------------'
-echo 'Loading the mqss env'
-echo '---------------------------------'
-t0=$(date +%s%N)
+t0_timestamp=$(date +"%a %d-%m-%Y %H:%M:%S.%3N")
+t0=$(date +%s%3N)
 source /home/sw/qis/wolpy/mqss/scripts/load-mqss-qoffload.sh
-t1=$(date +%s%N)
+t1_timestamp=$(date +"%a %d-%m-%Y %H:%M:%S.%3N")
+t1=$(date +%s%3N)
+echo "[SBATCH] $t0_timestamp: Start loading mqss-qoffload env"
+echo "[SBATCH] $t1_timestamp: Finished loading mqss-qoffload env"
+echo "[SBATCH] -------------------------------------------------"
+echo "[SBATCH] Elapsed time to load mqss-qoffload env (ms): $((t1 - t0))"
+echo "[SBATCH] -------------------------------------------------"
+echo ""
+
+t2_timestamp=$(date +"%a %d-%m-%Y %H:%M:%S.%3N")
+t2=$(date +%s%3N)
 source ~/export_conda_env.sh
-t2=$(date +%s%N)
-echo "Time to load mqss env (ms): $((t1 - t0))"
-echo "Time to load conda env (ms): $((t2 - t1))"
+t3_timestamp=$(date +"%a %d-%m-%Y %H:%M:%S.%3N")
+t3=$(date +%s%3N)
+echo "[SBATCH] -------------------------------------------------"
+echo "[SBATCH] Elapsed time to load user-conda env (ms): $((t3 - t2))"
+echo "[SBATCH] -------------------------------------------------"
+echo ""
 
 export MQSS_HPCQC_ENV=True
-t_start=$(date +%s%N)
-~/conda/x86_64/bin/python random_hamiltonian_h2_simulation.py --backend=QExa20 --maxiter=1
-t_end=$(date +%s%N)
-echo "Total execution time (ms): $((t_end - t_start))"
+t_start_timestamp=$(date +"%a %d-%m-%Y %H:%M:%S.%3N")
+t_start=$(date +%s%3N)
+~/conda/x86_64/bin/python random_hamiltonian_h2_simulation.py --backend=QExa20 --maxiter=2
+t_end_timestamp=$(date +"%a %d-%m-%Y %H:%M:%S.%3N")
+t_end=$(date +%s%3N)
+echo "[SBATCH] $t_start_timestamp: Start execution"
+echo "[SBATCH] $t_end_timestamp: Finished execution"
+echo "[SBATCH] -------------------------------------------------"
+echo "[SBATCH] Elapsed time to execute (ms): $((t_end - t_start))"
+echo "[SBATCH] -------------------------------------------------"
+echo ""
 
-echo '---------------------------------'
-echo 'Unloading the mqss env'
-echo '---------------------------------'
-t3=$(date +%s%N)
+t3_timestamp=$(date +"%a %d-%m-%Y %H:%M:%S.%3N")
+t3=$(date +%s%3N)
 source /home/sw/qis/wolpy/mqss/scripts/unload-mqss-qoffload.sh
-t4=$(date +%s%N)
-echo "Time to unload mqss env (ms): $((t4 - t3))"
+t4_timestamp=$(date +"%a %d-%m-%Y %H:%M:%S.%3N")
+t4=$(date +%s%3N)
+echo "[SBATCH] $t3_timestamp: Unloading mqss-qoffload env"
+echo "[SBATCH] $t4_timestamp: Finished unloading mqss-qoffload env"
+echo "[SBATCH] -------------------------------------------------"
+echo "[SBATCH] Elapsed time to unload mqss-qoffload env (ms): $((t4 - t3))"
+echo "[SBATCH] -------------------------------------------------"
+echo ""
